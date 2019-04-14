d716f264b4cc78027a74143bdbf7caf75f9ccaf4
Vector000 <33326900+Vector000@users.noreply.github.com>
2019-04-15 02:56:17 +0800

    refactor(autoUpdate): 不使用tsc-watch做自动更新，改为配合pm2使用
    pm2了解一下
    
    BREAKING CHANGE: 需要重新安装依赖包

--- bilive/plugins/autoUpdate/index.ts
+++ bilive/plugins/autoUpdate/index.ts
@@ -20,7 +20,7 @@ class AutoUpdate extends Plugin {
     if (cstMin === 0 && cstHour % 12 === 6) this._checkForUpdate()
   }
   // 根目录路径
-  private _dirname = __dirname + (process.env.npm_package_scripts_start === 'tsc-watch --onSuccess \"node build/app.js\"' ? '/../../..' : '/../..')
+  private _dirname = __dirname + '/../../..'
   /**
    * 检查当前应用目录是否有对应git仓库
    *
@@ -68,46 +68,6 @@ class AutoUpdate extends Plugin {
     return realBranch
   }
   /**
-   * 获取当前本地分支的commit hash
-   * 
-   * @private
-   */
-  private async getLocalCommitHash() {
-    return await this.execCMD('git rev-parse HEAD')
-  }
-  /**
-   * 获取远端文件
-   *
-   * @private
-   */
-  private async fetchRemote() {
-    return await this.execCMD('git fetch')
-  }
-  /**
-   * 获取对应远程分支的commit hash
-   * 
-   * @private
-   */
-  private async getRemoteCommitHash(branch: string) {
-    return await this.execCMD(`git rev-parse origin/${branch}`)
-  }
-  /**
-   * git merge
-   * 
-   * @private
-   */
-  private async gitMerge() {
-    return await this.execCMD('git merge')
-  }
-  /**
-   * 更新设置页面
-   * 
-   * @private
-   */
-  private async updateDocs() {
-    return await this.execCMD('npm run build:view')
-  }
-  /**
    * 检查更新
    * 
    * @private
@@ -115,26 +75,23 @@ class AutoUpdate extends Plugin {
    */
   private async _checkForUpdate() {
     const pathStatus = await this._checkPath(this._dirname)
-    if (!pathStatus) return tools.Log(`未发现git仓库，无法进行自动更新，建议查阅README.md进行重新安装！`)
+    if (!pathStatus) return tools.Log(`未发现git仓库，无法进行自动更新`)
     else {
       tools.Log(`正在查询更新......`)
       let branch = await this.gitBranch()
       if (branch === undefined || branch === '') return tools.ErrorLog(`获取本地分支信息失败`)
-      let localHash = await this.getLocalCommitHash()
+      let localHash = await this.execCMD('git rev-parse HEAD')
       if (localHash === undefined) return tools.ErrorLog(`获取本地commit信息失败`)
-      let fetchStatus = await this.fetchRemote()
+      let fetchStatus = await this.execCMD('git fetch')
       if (fetchStatus === undefined) return tools.ErrorLog(`获取最新版本失败`)
-      let remoteHash = await this.getRemoteCommitHash(branch)
+      let remoteHash = await this.execCMD(`git rev-parse origin/${branch}`)
       if (remoteHash === undefined) return tools.ErrorLog(`获取最新版本信息失败`)
-      if (localHash === remoteHash) {
-        tools.Log(`当前版本 (${branch}@${localHash.substr(0,8)}) 已是最新版本`)
-        this.updateDocs()
-      }
+      if (localHash === remoteHash) tools.Log(`当前版本 (${branch}@${localHash.substr(0,8)}) 已是最新版本`)
       else {
         tools.Log(`发现新版本 (${branch}@${remoteHash.substr(0,8)}) 10秒后开始更新 可能会重启数次`)
         tools.sendSCMSG(`发现新版本 即将进行自动升级`)
-        await tools.Sleep(10 * 1000)
-        await this.gitMerge()
+        await this.execCMD('git merge')
+        await this.execCMD('npm run build')
       }
     }
   }
